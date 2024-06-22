6b8f412f3e87bb60f45f3d5dd8c13913392b8060
EdgarERJ190 <edgar.omondi@strathmore.edu>
2024-06-23 01:09:35 +0300

    Delete all files in repository

--- DoctorConsultation.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const form = document.getElementById('consultation-form');
-
-    form.addEventListener('submit', function (event) {
-        event.preventDefault();
-        alert('Consultation booked successfully!');
-        form.reset();
-    });
-});
