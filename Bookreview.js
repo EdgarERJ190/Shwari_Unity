3b029de723154914556a458e30c38690fe380692
EDGAR CHARLES OMONDI <168529666+EdgarERJ190@users.noreply.github.com>
2024-06-23 00:58:01 +0300

    Delete Bookreview.js

--- Bookreview.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const submitButtons = document.querySelectorAll('.submit-btn');
-
-    submitButtons.forEach(button => {
-        button.addEventListener('click', () => {
-            alert('Review submitted!');
-        });
-    });
-});
